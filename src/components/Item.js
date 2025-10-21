a2d2de7ad53c3a79d7e28c60eb71740a7918fa65
Allan Ratemo <allanisratemo@gmail.com>
2025-10-21 07:02:16 +0300

    In cart button fixed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(false);
+
+  const handleAddToCart = () => {
+    setIsInCart(true);
+  };
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="in-cart">Add to Cart</button>
+      <button className="in-cart" onClick={handleAddToCart}>
+        {isInCart ? "In Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
